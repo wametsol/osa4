5e0d2222402ccb42f0c60cc171e73ae7cd4cd86b
wametsol <waltteri.metsola@gmail.com>
2018-02-25 22:14:45 +0200

    4.12
    Lisätty test_helper ja refakturoitu testejä

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,57 +2,9 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const { formatBlog, initialBlogs, nonExistingId, blogsInDB} = require('./test_helper')
 
-const initialBlogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }  
-  ]
+
   beforeAll(async () => {
       await Blog.remove({})
       const blogObjects = initialBlogs.map(blog => new Blog(blog))
@@ -61,17 +13,23 @@ const initialBlogs = [
     
   })
 test('blogs are returned as json', async () => {
+  const blogsInDb = await blogsInDB()
     await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+
+    
 })
 
 test('there are 5 blogs', async () => {
+    const blogsDB = await blogsInDB()
+
     const response = await api
     .get('/api/blogs')
+    .expect('Content-Type', /application\/json/)
 
-    expect(response.body.length).toBe(initialBlogs.length)
+    expect(response.body.length).toBe(blogsDB.length)
 })
 test('blogs contains title: type wars', async () => {
     const res = await api
@@ -81,6 +39,7 @@ test('blogs contains title: type wars', async () => {
     expect(contents).toContain('Type wars')
 })
 test('a valid blog can be addded', async () => {
+  const blogsAtStart = await blogsInDB()
   const newBlog = {
     _id: "5a422bc61b54a62323541523",
       title: "Tester blog",
@@ -95,12 +54,12 @@ test('a valid blog can be addded', async () => {
   .expect(200)
   .expect('Content-Type', /application\/json/)
 
-  const res = await api
-  .get('/api/blogs')
+  const blogsAfter = await blogsInDB()
+  expect(blogsAfter.length).toBe(blogsAtStart.length+1)
 
-  const contents = res.body.map(r => r.title)
+  const contents = blogsAfter.map(r => r.title)
 
-  expect(res.body.length).toBe(initialBlogs.length+1)
+  
   expect(contents).toContain('Tester blog')
 })
 
@@ -118,11 +77,10 @@ test('if likes is undefined, it is set to 0', async () => {
   .expect(200)
   .expect('Content-Type', /application\/json/)
 
-  const res = await api
-  .get('/api/blogs')
+  const res = await blogsInDB()
 
 
-  const content = res.body.find(r => 
+  const content = res.find(r => 
   r.title === 'Tester blog2')
   expect(content.likes).toBe(0)
   
@@ -135,8 +93,7 @@ test('if title & url is undefined, return 400 Bad request', async () => {
       likes: "100",
       __v: 0
   }
-  const blogit = await api
-  .get('/api/blogs')
+  const blogit = await blogsInDB()
 
   await api
   .post('/api/blogs')
@@ -144,11 +101,10 @@ test('if title & url is undefined, return 400 Bad request', async () => {
   .expect(400)
   .expect('Content-Type', /application\/json/)
 
-  const res = await api
-  .get('/api/blogs')
+  const res = await blogsInDB()
 
 
-  expect(res.body.length).toBe(blogit.body.length)
+  expect(res.length).toBe(blogit.length)
   
 })
 
